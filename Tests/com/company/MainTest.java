eadd3a5831a25760f878abde41403233ebe734cc
Kirill_Kiselev <kvladimirovich10@gmail.com>
2016-09-12 20:52:10 +0300

    Generic

--- Tests/com/company/MainTest.java
+++ Tests/com/company/MainTest.java
@@ -6,22 +6,21 @@ import static org.junit.Assert.*;
 
 import java.util.Random;
 
-//generic
-//хэш таблицы
 
 public class MainTest {
 
-    //private Main sum = new Main();
-    private Random random = new Random();
-    private int EVENSIZE = 1000;
 
-    private void createRandArray(int[] arr) {
-        for (int i : arr) {
-            arr[i] = random.nextInt();
+    private Random random = new Random();
+    private int EVENSIZE = 10;
+
+    private Integer[] createRandArray(Integer[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(10);
         }
+        return arr;
     }
 
-    private boolean sortfunc(int[] arr) {
+    private boolean sortFunctionCheck(Integer[] arr) {
 
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i + 1]) {
@@ -34,21 +33,22 @@ public class MainTest {
     @Test
     public void lonelyArrayTest() throws Exception {
 
-        int[] arr1 = {1};
-        int a = arr1[0];
+        Integer[] arr1 = {1};
+        Integer a = arr1[0];
 
-        Main.cocktailsort(arr1);
+        Main.cocktailSort(arr1);
         assertEquals(a, arr1[0]);
     }
 
     @Test
-    public void sameTest() throws Exception {
-        int[] arr = new int[EVENSIZE];
+    public void sameElemetArrayTest() throws Exception {
+        Integer[] arr = new Integer[EVENSIZE];
         int i;
+
         for (i = 0; i < arr.length; i++) {
             arr[i] = 42;
         }
-        Main.cocktailsort(arr);
+        Main.cocktailSort(arr);
 
         boolean check;
         for (i = 0; i < arr.length - 1; i++) {
@@ -63,13 +63,13 @@ public class MainTest {
     public void oddTest() throws Exception {
 
 
-        int[] arr = new int[EVENSIZE + 1];
+        Integer[] arr = new Integer[EVENSIZE+1];
 
-        createRandArray(arr);
+        arr = createRandArray(arr);
 
-        Main.cocktailsort(arr);
+        Main.cocktailSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
 
 
     }
@@ -77,74 +77,73 @@ public class MainTest {
     @Test
     public void evenTest() throws Exception {
 
-        int[] arr = new int[EVENSIZE];
+        Integer[] arr = new Integer[EVENSIZE];
 
         createRandArray(arr);
 
-        Main.cocktailsort(arr);
+        Main.cocktailSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
     @Test
     public void AlresdySortedTest() throws Exception {
 
-        int i;
-        int[] arr = new int[EVENSIZE];
+        Integer[] arr = new Integer[EVENSIZE];
 
-        for (i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 42;
         }
-        Main.cocktailsort(arr);
+        Main.cocktailSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
     @Test
     public void alresdySortedBackTest() throws Exception {
 
-        int i;
-        int[] arr = new int[EVENSIZE];
 
-        for (i = 0; i < arr.length; i++) {
+        Integer[] arr = new Integer[EVENSIZE];
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = arr.length - i;
         }
-        Main.cocktailsort(arr);
+        Main.cocktailSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
     @Test
     public void negativeTest() throws Exception {
 
-        int i;
-        int[] arr = new int[EVENSIZE];
 
-        for (i = 0; i < arr.length; i++) {
+        Integer[] arr = new Integer[EVENSIZE];
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = (-1) * random.nextInt();
         }
-        Main.cocktailsort(arr);
+        Main.cocktailSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
     @Test
     public void hugeTest() throws Exception {
 
-        int[] arr = new int[EVENSIZE * 100];
+        Integer[] arr = new Integer[EVENSIZE * 100];
 
         createRandArray(arr);
-        Main.cocktailsort(arr);
+        Main.cocktailSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
 
     @Test
     public void mergeSortlonelyArrayTest() throws Exception {
 
-        int[] arr1 = {1};
-        int a = arr1[0];
+        Integer[] arr1 = {1};
+        Integer a = arr1[0];
 
         Main.mergeSort(arr1);
         assertEquals(a, arr1[0]);
@@ -152,15 +151,15 @@ public class MainTest {
 
     @Test
     public void mergeSortsameTest() throws Exception {
-        int[] arr = new int[EVENSIZE];
-        int i;
-        for (i = 0; i < arr.length; i++) {
+        Integer[] arr = new Integer[EVENSIZE];
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = 42;
         }
         Main.mergeSort(arr);
 
         boolean check;
-        for (i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] != arr[i + 1]) {
                 check = false;
                 assertTrue(check);
@@ -171,14 +170,13 @@ public class MainTest {
     @Test
     public void mergeSortoddTest() throws Exception {
 
-
-        int[] arr = new int[EVENSIZE + 1];
+        Integer[] arr = new Integer[EVENSIZE + 1];
 
         createRandArray(arr);
 
         Main.mergeSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
 
 
     }
@@ -186,66 +184,66 @@ public class MainTest {
     @Test
     public void mergeSortevenTest() throws Exception {
 
-        int[] arr = new int[EVENSIZE];
+        Integer[] arr = new Integer[EVENSIZE];
 
         createRandArray(arr);
 
         Main.mergeSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
     @Test
     public void mergeSortAlresdySortedTest() throws Exception {
 
-        int i;
-        int[] arr = new int[EVENSIZE];
 
-        for (i = 0; i < arr.length; i++) {
+        Integer[] arr = new Integer[EVENSIZE];
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 42;
         }
         Main.mergeSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
     @Test
     public void mergeSortalresdySortedBackTest() throws Exception {
 
-        int i;
-        int[] arr = new int[EVENSIZE];
 
-        for (i = 0; i < arr.length; i++) {
+        Integer[] arr = new Integer[EVENSIZE];
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = arr.length - i;
         }
         Main.mergeSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
     @Test
     public void mergeSortnegativeTest() throws Exception {
 
         int i;
-        int[] arr = new int[EVENSIZE];
+        Integer[] arr = new Integer[EVENSIZE];
 
         for (i = 0; i < arr.length; i++) {
             arr[i] = (-1) * random.nextInt();
         }
         Main.mergeSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
     @Test
     public void mergeSorthugeTest() throws Exception {
 
-        int[] arr = new int[EVENSIZE * 100];
+        Integer[] arr = new Integer[EVENSIZE * 100];
 
         createRandArray(arr);
         Main.mergeSort(arr);
 
-        assertTrue("not sorted", sortfunc(arr));
+        assertTrue("not sorted", sortFunctionCheck(arr));
     }
 
 }
---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,102 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Main {
+
+    public static void createArrayFunc(Integer[] arr1) {
+
+        Random random = new Random();
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = random.nextInt(100);
+        }
+    }
+
+
+    private static <T> void swap(T[] array,int i){
+        T swap = array[i + 1];
+        array[i + 1] = array[i];
+        array[i] = swap;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Integer[] arr1 = new Integer[10];
+
+        createArrayFunc(arr1);
+        cocktailSort(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+        createArrayFunc(arr1);
+        mergeSort(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+    }
+
+
+
+
+    public static <T extends Comparable<T>> void cocktailSort(T[] array) {
+
+        int left = 0;
+        int right = array.length - 1;
+
+
+        do {
+
+            for (int i = left; i < right; i++) {
+                if (array[i].compareTo(array[i+1])>=0)
+                    swap(array,i);
+            }
+            right--;
+            for (int i = right; i > left; i--) {
+                if (array[i].compareTo(array[i+1])>=0)
+                    swap(array,i);
+            }
+            left++;
+        } while (left <= right);
+
+    }
+
+
+
+    public static <T extends Comparable<T>> void mergeSort(T[] a) {
+        mergeSort(a, a, 0, a.length - 1);
+    }
+
+
+    private static <T extends Comparable<T>> void mergeSort(T[] a, T[] tmp, int left, int right) {
+        if (left < right) {
+            int center = (left + right) / 2;
+            mergeSort(a, tmp, left, center);
+            mergeSort(a, tmp, center + 1, right);
+            merge(a, tmp, left, center + 1, right);
+        }
+    }
+
+
+    private static <T extends Comparable<T>> void merge(T[] a, T[] tmp, int left, int right, int rightEnd) {
+        int leftEnd = right - 1;
+        int k = left;
+        int num = rightEnd - left + 1;
+
+        while (left <= leftEnd && right <= rightEnd)
+            if (a[left].compareTo(a[right])<=0)
+                tmp[k++] = a[left++];
+            else
+                tmp[k++] = a[right++];
+
+        while (left <= leftEnd)
+            tmp[k++] = a[left++];
+
+        while (right <= rightEnd)
+            tmp[k++] = a[right++];
+
+        for (int i = 0; i < num; i++, rightEnd--)
+            a[rightEnd] = tmp[rightEnd];
+    }
+}
